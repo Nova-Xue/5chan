d800bffae487303ea65879d7e297a6465ab21106
Nova <xueshengyuan1118@hotmail.com>
2019-08-01 12:23:46 -0400

    Set up server

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 3001;
+var db = require("./models");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  app.use(routes);
+// Routes
+// =============================================================
+
+db.sequelize.sync({force:true}).then(()=>{
+    app.listen(PORT,()=>{
+        console.log("App listening on PORT " + PORT);
+    });
+});
