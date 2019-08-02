09f49332bb0aac7cd5833cbe1ebe2a77ff0adadb
Nova <xueshengyuan1118@hotmail.com>
2019-08-01 23:14:52 -0400

    Create user routes with sequelize model User

--- controllers/userControlloer.js
+++ controllers/userControlloer.js
@@ -1,20 +1,30 @@
 const db = require("../models");
 module.exports = (app) => {
-    app.post("/user",(req,res)=>{
-       //create new user
-       //username 
-       //password
-       //email
-       //location 
-    });
-    app.put("/user/:id",(req,res)=>{
-        //update user
-        //username 
-        //password
-        //email
-        //location 
-     });
-     app.delete("/user/:id",(req,res)=>{
-        //id === delete user by id
-      });
+   app.post("/user", (req, res) => {
+      db.User.create(req.body)
+         .then(data => console.log(data))
+         .catch(err => console.log(data));
+   });
+   app.put("/user/:id", (req, res) => {
+      db.User.update({
+         email: req.params.email,
+         location: req.params.location
+         }, {
+            where: {
+               uid: req.params.id
+            }
+         })
+         .then(data => console.log(data))
+         .catch(err => console.log(data));
+   });
+   app.delete("/user/:id", (req, res) => {
+      //id === delete user by id
+      db.User.destory({
+         where : {
+            uid : req.params.id
+         }
+      })
+         .then(data => console.log(data))
+         .catch(err => console.log(data));
+   });
 };
